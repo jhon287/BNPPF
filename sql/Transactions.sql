ff9b7df54429b120297246abbd7fd02767cc1180
Jonathan Sabbe <sabbe.jonathan@gmail.com>
2016-11-06 22:35:38 +0100

    CREATE TABLE syntax for Transactions

---
+++ sql/Transactions.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `Transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reference` text NOT NULL,
+  `dateOf` text NOT NULL,
+  `amount` double DEFAULT NULL,
+  `currency` text NOT NULL,
+  `typeOf` text NOT NULL,
+  `comment` text NOT NULL,
+  `account` text NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
