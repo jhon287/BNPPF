4a5ee14170019dbf97a800278edd9df549a07006
Jonathan Sabbe <sabbe.jonathan@gmail.com>
2022-03-17 17:07:08 +0100

    feat: Refactor BNPPF code and make it working with new BNPPF CSV format

--- banking.py
+++ banking.py
@@ -1,5 +1,5 @@
-import glob
-import re
+#!/usr/bin/env python3
+
 import sys
 import os
 import configparser
@@ -20,12 +20,12 @@ try:
         for key in config[section]:
             settings[section][key] = config[section][key]
 except Exception as e:
-    print("OS error: {0}".format(e))
+    print(f'OS error: {e}')
     sys.exit(1)
 
 for directory in [settings['directory']['in'], settings['directory']['done']]:
     if not os.path.exists(directory):
-        print("Creating '{}'...".format(directory))
+        print(f"Creating '{directory}'...")
         os.makedirs(directory)
 
 try:
@@ -37,32 +37,42 @@ try:
         database=settings['database']['name']
     )
     cur = con.cursor()
-    for f in glob.glob(settings['directory']['in'] + '/*'):
+
+    csv_version = None
+
+    for f in sorted(os.listdir(settings['directory']['in'])):
+        # Full path
+        f = f"{settings['directory']['in']}/{f}"
+
+        # Check if file in really a file and that is exists
         if not os.path.isfile(f):
             continue
-        print("Current: " + f)
+
+        print(f'Current: {f}')
+
         lines = [line.rstrip('\n') for line in
                  open(f, 'r', encoding='utf8', errors='ignore')]
-        first_line = True
+
         for line in lines:
-            transaction = bnppf.BNPPF()
-            if first_line:
-                if re.search("CONTREPARTIE DE (L'OPERATION|LA TRANSACTION)",
-                             line, re.IGNORECASE):
-                    counterparty = True
-                else:
-                    counterparty = False
-                first_line = False
-                print("Counterparty: " + str(counterparty))
+            # Empty line ?
+            if not line.strip():
                 continue
-            if not line.strip():  # Empty line ?
+
+            # First line -> CSV header
+            if csv_version is None:
+                csv_version = bnppf.get_csv_version(line)
                 continue
-            if not transaction.parse(line=line, format='csv',
-                                     counterparty=counterparty):
+
+            transaction = bnppf.BNPPF(csv_version=csv_version)
+
+            # Parse CSV transaction line
+            if not transaction.parse(line=line, format='csv'):
                 print('ERROR:', transaction.get_all())
                 sys.exit(1)
+            # Skipping invalid sequence number
             elif transaction.get_ref() is None:
-                continue  # Skipping invalid sequence number
+                continue
+
             account = transaction.get_account()
             sql = 'CALL addTransaction("{ref}", "{date}", {amount}, ' \
                   '"{currency}", "{type}", "{detail}", "{account}");'.format(
@@ -78,17 +88,21 @@ try:
             if account not in accounts:
                 accounts[account] = {'count': 0}
             accounts[account]['count'] += 1
+
+        # Move CSV file to done folder
         os.rename(f, f.replace(settings['directory']['in'],
                                settings['directory']['done']))
-        con.commit()  # Commit changes after each file
+
+        # Commit changes after each file
+        con.commit()
+        csv_version = None
     print('--------------------------------------------------------')
     print('Transactions Summary:')
     for k, v in accounts.items():
-        print(' - {account} -> {transactions}'.format(
-                account=k, transactions=v['count']))
+        print(f" - {k} -> {v['count']}")
     print('--------------------------------------------------------')
 except Exception:
-    print("Unexpected error:", sys.exc_info())
+    print('Unexpected error:', sys.exc_info())
     sys.exit(1)
 finally:
     if con:
